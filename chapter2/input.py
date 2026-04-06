1096d9cce86f9a53f0e97bba6a0242a420c29520
Zakariya Mohamed <zakariyamohamed1987@gmail.com>
2026-04-06 07:56:35 -0600

    lessons for python

---
+++ chapter2/input.py
@@ -0,0 +1,26 @@
+# first_name = input("what's your name?")
+# print("your name is" + first_name)
+
+
+# age = int (input ("what's your age?"))
+# print (age + 5)
+
+
+# age = int ("29")
+# print(age + 5) 
+
+
+# # converting float to int 
+# num = 4.7 
+# int_num = int(num)
+# print (int_num) 
+
+
+# first_name = "Zakariya"
+# print (f"hello {first_name}") 
+
+
+favorite_movie = "Big Hero 6"
+birth_date = 2007 
+price_snake = "12.32"
+enjoy_programming = True 
---
+++ chapter2/variables.py
@@ -0,0 +1,52 @@
+# zakariya mohamed 21 True
+
+# mubaarak ahmed 19 false
+
+
+# first_name1 = "Zakariya" # string
+# last_name1 = "Mohamed" # string
+# age1 = 21 # int / float(if decimal)
+# isstudent1 = True # boolean
+# print (first_name1)
+# print (last_name1)
+# print(age1)
+
+
+
+# first_name2 = "Mubaarak" 
+# last_name2 = "Ahmed"
+# age2 =19
+# isstudent2 = False
+# print (first_name2)
+# print (last_name2)
+# print (age2)
+
+
+# num_sweets = 10
+# num_kids = 5
+# sum = num_sweets / num_kids
+
+# print(num_sweets / num_kids)
+# print(sum)
+
+
+
+
+city = """Minneapolis 
+USA"""
+print (city) 
+
+
+
+
+
+# age = 25.4
+# print (type(age)) 
+
+
+# is_student = True 
+# print (type(is_student)) 
+
+
+
+
