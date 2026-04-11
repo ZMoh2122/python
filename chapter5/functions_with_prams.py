66c8381b11bfdd23fa85953aec3ef07c518aa53e
Zakariya Mohamed <zakariyamohamed1987@gmail.com>
2026-04-11 08:26:09 -0600

    lessons for python

---
+++ chapter5/functions_with_prams.py
@@ -0,0 +1,53 @@
+# def welcome(): 
+#     print("Welcome to python!")
+
+# welcome()    
+
+# calling/executing/running/invoking
+
+
+# def greet():
+#     print("Hello, welcome to Python!")
+
+# # Calling the function
+# greet()
+
+
+# def add_numbers(param1,param2,param3):
+#     return int(param1 + param2 + param3) 
+
+# num1 = 5
+# num2 = 10
+
+# result1 = add_numbers(num2, num2, num2)
+# result2 = add_numbers(num1, num1, num1)    
+# print(result1)
+# print(result2)
+
+
+
+# def multiply_numbers(param1,param2,param3):
+#     return int(param1 * param2 * param3) 
+
+# num1 = 5
+# num2 = 10
+
+# result1 = multiply_numbers(num1, num1, num1)
+# result2 = multiply_numbers(num2, num2, num2)    
+# print(result1)
+# print(result2)
+
+
+
+# def person_details(name, age, profession):
+#     greeting = f"Hello, {name}!"
+#     description = f"{name} is {age} years old and works as a {profession}."
+#     return greeting, description
+
+# # Call the function
+# greeting, description = person_details("Alice", 30, "Software Engineer")
+
+# # Print the returned values
+# print(greeting)       # Output: Hello, Alice!
+# print(description)    # Output: Alice is 30 years old and works as a Software Engineer.
+
---
+++ chapter6/lists.py
@@ -0,0 +1,56 @@
+#Creating a list
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits[0])
+
+
+# numbers = [1, 2, 3, 4]
+# print(numbers[3])
+
+
+
+
+# teachers = ["Smith", "Thomas", "James", "Zakey"]
+# teachers[0] = "Osman"
+# is_married = [True, False, True, False]
+# print(teachers)
+# # print(is_married[1])
+# # print(teachers[-1])
+
+ 
+# teachers = ["Smith", "Thomas", "James", "Zakey"]
+# teachers.append ("Bushra")
+# print(teachers)
+
+
+# teachers = ["Smith", "Thomas", "James", "Zakey"]
+# teachers.insert(0, "Williams")
+# print(teachers)
+
+
+# teachers = ["Smith", "Thomas", "James", "Zakey"]
+# teachers.remove("Smith")
+# print(teachers)
+
+
+# teachers = ["Smith", "Thomas", "James", "Zakey"]
+# removed = teachers.pop(0)
+# print(teachers) 
+# print(removed)
+
+
+# teachers = ["Smith", "Thomas", "James", "Zakey"]
+# teachers.clear()
+# print(teachers)
+
+
+
+for i in range (13):
+    print(i)
+
+
+
+
+
+# mixed = [1, "apple", True]
+
+# print(fruits)  # Output: ['apple', 'banana', 'cherry']
