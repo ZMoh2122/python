f70731ea53917dc18eda98e53ea0d2cdfa1935b8
Zakariya Mohamed <zakariyamohamed1987@gmail.com>
2026-04-12 09:36:22 -0500

    lessons for python

---
+++ chapter6/data_structures.py
@@ -0,0 +1,74 @@
+#Lists
+# fruits = ["apple", "banana", "cherry"]
+# fruits.append("orange")
+# print(fruits) # Output: ['apple', 'banana', 'cherry', 'orange']
+
+
+# #Tuples
+# coordinates = (10, 20, 30)
+# print(coordinates[1]) # Output: 20
+
+
+# students = {"Zak", "Muba", "Ray"}
+# students.add("Von")
+# print(students)
+
+
+# student = {"name": "Zaky", "age": 18}
+# student["School"] = "Metro Schools"
+# del student["age"]
+# school = student.get("School")
+# print(student)  # Output: Zaky
+
+
+
+# stack = []
+# stack.append("Harry Potter")  # Push
+# stack.append("Big Nate")
+# print(stack)
+# print(stack.pop())  # Output: 2
+# print(stack)
+
+
+# num1 = 10 
+# num2 = 0
+# print(num1/num2)
+
+
+# try:
+#     # Code that might raise an exception
+#     result = 10 / 0
+#     print(result)
+# except ZeroDivisionError:
+#     # Code to execute if an exception occurs
+#     print("You cannot divide by zero!")
+
+
+
+# try:
+#     file = open("nonexistent_file.txt", "r")
+# except FileNotFoundError:
+#     print("File not found!")
+# finally:
+#     print("This will always run.")
+# # print("This will run even if an exception occurs.")
+
+
+
+# try:
+#     age = 6
+#     if age < 0:
+#      raise ValueError("Age cannot be negative!")
+# except ValueError as e:
+#     print("Please enter the correct age, Please and thank you")
+
+
+x = -1
+assert x > 0, "x must be positive!"
+
+    
+
+
+
+
+
---
+++ chapter7/file_practice.py
@@ -0,0 +1,7 @@
+# Step 1: Open a file in 'w' mode
+with open("example.txt", "w") as file:
+ file.write("Zakariya")
+ file.write("Mubaarak")
+
+# Step 3: Close the file
+file.close()
